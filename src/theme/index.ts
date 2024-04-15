f6000cdc402fc816d9dc58a9832078284c973ea3
5iveemmy <ainemmanuel87@gmail.com>
2024-04-15 21:41:06 +0100

    chore: create chakra custom theme

---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+import { buttonTheme } from "./button";
+import { colors } from "./colors";
+
+export const theme = extendTheme({
+  colors,
+  components: {
+    Button: buttonTheme,
+  },
+});
